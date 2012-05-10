5224471f6921fbcce2336c735c177a9fdb80b61c
Christophe Faivre <christophe@faivre.co.za>
2012-05-10 23:21:46 +0200

    WIP

--- app/lib/proxmox_client.rb
+++ app/lib/proxmox_client.rb
@@ -1,9 +1,26 @@
  class ProxmoxClient
 
   def self.node_status(options={})
+    options.required! :node
     @auth = {:username => ProxmoxSettings.username, :password => ProxmoxSettings.password}
-    response = HTTParty.get(ProxmoxSettings.auth_url_template, :basic_auth => @auth, :query => options)
-    return true if response.code == 200
+    response = HTTParty.get(ProxmoxSettings.node_status_template, :basic_auth => @auth, :query => options)
+    return response.body if response.code == 200
+    return false
+  end
+
+  def self.node_storage(options={})
+    options.required! :node
+    @auth = {:username => ProxmoxSettings.username, :password => ProxmoxSettings.password}
+    response = HTTParty.get(ProxmoxSettings.node_storage_template, :basic_auth => @auth, :query => options)
+    return response.body if response.code == 200
+    return false
+  end
+
+  def self.virtual_server_create(options={})
+    options.required! :node, :vmid
+    @auth = {:username => ProxmoxSettings.username, :password => ProxmoxSettings.password}
+    response = HTTParty.get(ProxmoxSettings.virtual_server_create_template, :basic_auth => @auth, :query => options)
+    return response.body if response.code == 200
     return false
   end
 
--- app/models/node.rb
+++ app/models/node.rb
@@ -2,23 +2,34 @@ class Node
   include Mongoid::Document
   field :name, :type => String
   field :ip, :type => String
+  field :storage_avilable, :type => Double
+  field :memory_available, :type > Double
   embeds_many :virtual_servers
   accepts_nested_attributes_for :virtual_servers, :reject_if => lambda{|a| a[:name].blank? && a[:ip].blank? && a[:belongs_to].blank? }, :allow_destroy => true
 
-  def self.available_storage
-    self.storage_usage[]
+  def self.update_storage_available
+    self.storage_avilable = self.actual_storage_usage["avail"]
   end
 
-  def self.available_memory
+  def self.update_memory_available
+    self.memory_available = self.actual_memory_usage["free"]
+  end
+
+  def provision_virtual_server(options={})
+    options.required! :storage_size, :memory_size
+    if options[:storage_size] < self.storage_available && options[:memory_size] < self.memory_available
+      ProxmoxClient.virtual_server_create(options)
+    end
   end
 
   private
 
-  def self.storage_usage
+  def self.actual_storage_usage
+    ProxmoxClient.node_storage.find { |node_storage| node_storage["storage"] == "VM_Storage" }
   end
 
-  def self.memory_usage
-    
+  def self.actual_memory_usage
+     ProxmoxClient.node_status["data"]["memory"]
   end
 
 end
